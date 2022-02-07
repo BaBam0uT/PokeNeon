9e41ed205fb07fc2f34a6dbc5bd792b00cdcc836
Valentin DEDET <valentin.dedet@hotmail.fr>
2022-02-07 02:32:04 +0100

    Ajout Page Accueil

---
+++ PokeNeon/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokeNeon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Onglets();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
